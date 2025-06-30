5959818d787ec6bf8bb5b49596d14a5ebb40f33f
DeemoHarlos <cces4415@gmail.com>
2025-06-30 10:24:37 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+const githubPagesEntry = [
+  'video', 'song', 'wishes', 'meet', 'arts', 'fanfic',
+  'signatures', 'fans', 'archives', 'prize', 'credit',
+]
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    viteStaticCopy({
+      targets: githubPagesEntry.map(r => ({
+        src: 'index.html',
+        dest: r,
+      }))
+    })
   ],
   build: {
     outDir: './docs',
